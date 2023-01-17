a68d8b6945e4f8646336d10c3eed0affdecc64ff
RAFAELE L FILIPPIS <>
2023-01-17 18:25:51 -0300

    enhance error page adding navbar

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,25 @@
 import { useRouteError } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Container from "./components/UI/Container/Container";
+import GlobalStyle from "./styles/globalStyles";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Container>
+        <div id="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>{error.statusText || error.message}</i>
+          </p>
+        </div>
+      </Container>
+    </>
   );
 }
